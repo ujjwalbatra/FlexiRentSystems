be486f6a18e900899b93a50dd77431315919e1ea
Ujjwal Batra <ujjwalbatra2509@gmail.com>
2018-09-11 22:29:01 +1000

    view property complete revamp, removed dialog

--- src/view/ViewProperty.java
+++ src/view/ViewProperty.java
@@ -7,14 +7,17 @@
 
 package view;
 
-import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
-import model.RentalProperty;
+import javafx.stage.Stage;
 
 /*
  * this class is used to generate view property UI, when
@@ -23,27 +26,32 @@ import model.RentalProperty;
  */
 
 public class ViewProperty {
+
+    private Stage stage;
+    private VBox completeUI;
+    private HBox functionBtns;
+
+    public ViewProperty() {
+        this.stage = new Stage();
+        this.completeUI = new VBox();
+        this.functionBtns = new HBox();
+    }
+
     public void generateViewPropertyUI() {
-        Dialog<RentalProperty> dialog = new Dialog();
 
         //block user interaction with other windows, until this window has been taken care of
-        dialog.initModality(Modality.APPLICATION_MODAL);
-        dialog.setTitle("Property Viewer");
-
-        // Set the button types.
-        ButtonType closeBtn = new ButtonType("Close", ButtonBar.ButtonData.CANCEL_CLOSE);
-        dialog.getDialogPane().getButtonTypes().addAll(closeBtn);
+        this.stage.initModality(Modality.APPLICATION_MODAL);
+        this.stage.setTitle("Property Viewer");
 
         GridPane rentalPropertyDetails = new GridPane();
         GridPane rentalRecordDetails = new GridPane();
 
         ScrollPane scrollPane = new ScrollPane();
         scrollPane.setContent(rentalRecordDetails);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-        scrollPane.setPrefSize(115, 100);
+        scrollPane.setPrefSize(700, 200);
 
-        Image image = new Image(this.getClass().getResource("images/sampleHouse1.png").toString(), 350, 350, true, true);
+        Image image = new Image(this.getClass().getResource("images/sampleHouse1.png").toString(), 300, 300, true, true);
         ImageView imageView = new ImageView(image);
 
         Label streetNumber = new Label("Street Number : ");
@@ -64,14 +72,10 @@ public class ViewProperty {
         Label rentalrec7 = new Label("record : ");
         Label rentalrec8 = new Label("record : ");
 
-        Button rentBtn = new Button("Rent");
-        Button returnPropertyBtn = new Button("Return");
+        Button rentBtn = new Button("Rent Property");
+        Button returnPropertyBtn = new Button("Return Property");
         Button performMaintenanceBtn = new Button("Perform Maintenance");
         Button completeMaintenanceBtn = new Button("Complete Maintenance");
-
-        rentalPropertyDetails.setHgap(10);
-        rentalPropertyDetails.setVgap(10);
-        rentalPropertyDetails.setPadding(new Insets(20, 20, 20, 20));
 
         rentalRecordDetails.add(rentalrec1, 0, 0);
         rentalRecordDetails.add(rentalrec2, 0, 1);
@@ -82,7 +86,7 @@ public class ViewProperty {
         rentalRecordDetails.add(rentalrec7, 0, 6);
         rentalRecordDetails.add(rentalrec8, 0, 7);
 
-        rentalPropertyDetails.add(imageView, 0, 0, 4, 2);
+
         rentalPropertyDetails.add(streetNumber, 0, 4, 2, 1);
         rentalPropertyDetails.add(streetName, 0, 5, 2, 1);
         rentalPropertyDetails.add(suburb, 0, 6, 2, 1);
@@ -91,26 +95,30 @@ public class ViewProperty {
         rentalPropertyDetails.add(description, 0, 9, 2, 2);
         rentalPropertyDetails.add(propertyStatus, 0, 11, 2, 1);
         rentalPropertyDetails.add(lastMaintenanceDate, 0, 12, 2, 1);
-        rentalPropertyDetails.add(scrollPane, 0, 13, 4, 3);
 
-        rentalPropertyDetails.add(rentBtn, 0, 16);
-        rentalPropertyDetails.add(returnPropertyBtn, 1, 16);
-        rentalPropertyDetails.add(performMaintenanceBtn, 2, 16);
-        rentalPropertyDetails.add(completeMaintenanceBtn, 3, 16);
+        this.functionBtns.getChildren().addAll(rentBtn, returnPropertyBtn, performMaintenanceBtn, completeMaintenanceBtn);
+        this.functionBtns.setSpacing(25);
 
-        GridPane.setHalignment(imageView, HPos.CENTER);
-        GridPane.setHalignment(rentBtn, HPos.CENTER);
-        GridPane.setHalignment(returnPropertyBtn, HPos.CENTER);
-        GridPane.setHalignment(performMaintenanceBtn, HPos.CENTER);
-        GridPane.setHalignment(completeMaintenanceBtn, HPos.CENTER);
+        this.completeUI.setPadding(new Insets(20,20,20,20));
+        this.completeUI.setSpacing(15);
 
-        //styling the dialog box
-        DialogPane dialogPane = dialog.getDialogPane();
-        dialogPane.getStylesheets().add(getClass().getResource("css/StyleUI.css").toExternalForm());
-        dialogPane.getStyleClass().add("viewPropertyDialog-pane");
+        rentalPropertyDetails.setVgap(5);
 
-        dialog.getDialogPane().setContent(rentalPropertyDetails);
-        dialog.showAndWait();
+        this.completeUI.getChildren().addAll(imageView, rentalPropertyDetails, scrollPane, this.functionBtns);
+        this.completeUI.setAlignment(Pos.CENTER);
+        rentalPropertyDetails.setAlignment(Pos.CENTER_LEFT);
+        this.functionBtns.setAlignment(Pos.CENTER);
+
+        //styling the page
+        completeUI.getStylesheets().add(getClass().getResource("css/StyleUI.css").toExternalForm());
+        completeUI.getStyleClass().add("viewPropertyDialog-pane");
+        rentalPropertyDetails.getStylesheets().add(getClass().getResource("css/StyleUI.css").toExternalForm());
+        rentalPropertyDetails.getStyleClass().add("viewPropertyDialog-pane");
+        rentalRecordDetails.getStylesheets().add(getClass().getResource("css/StyleUI.css").toExternalForm());
+        rentalRecordDetails.getStyleClass().add("viewPropertyDialog-pane");
+
+        this.stage.setScene(new Scene(completeUI, 800, 750));
+        this.stage.showAndWait();
 
     }
 }
